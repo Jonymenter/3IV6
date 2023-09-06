7b023d6edda055c78801ed9654498699c2bafb63
Jonymenter <136421296+Jonymenter@users.noreply.github.com>
2023-09-06 16:46:25 -0600

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,7 +1,7 @@
 function ir(){
 
 var c=8681800405;
-var	u="3IV6-Cecyt-9";
+var	u="3IV6Cecyt9";
  if(document.form.password.value==c && document.form.login.value==u){
  alert ("Bienvenid@ a 3IV6");
 window.location="inicio.html";}
