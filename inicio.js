b76fcb14d62a73d9a799f73bfd18fdc02588b0d9
Jonymenter <136421296+Jonymenter@users.noreply.github.com>
2023-09-06 16:20:22 -0600

    First Commit

---
+++ inicio.js
@@ -0,0 +1,12 @@
+function ir(){
+
+var c=123456;
+var	u="Nombre";
+ if(document.form.password.value==c && document.form.login.value==u){
+ alert ("Bienvenid@ a 3IV6");
+window.location="inicio.html";}
+
+else{
+alert("Porfavor ingrese, nombre y usuario correctos.");
+}
+}
